8280df69496f4d4c44f7b5700a1f23f16b52a495
raushan <kumarraushan2615@gmail.com>
2025-03-16 22:05:24 +0530

    created-structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=''>
+      hello from vite and tailwind
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Head.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <div className=''>
+    <img src=''></img>
+    </div>
+  )
+}
+
+export default Head
